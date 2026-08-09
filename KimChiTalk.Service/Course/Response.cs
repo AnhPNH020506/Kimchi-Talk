adc956b97ea6e79d82d87d45b6c950f5777608d5
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-09 19:52:30 +0700

    update code entity course

--- KimChiTalk.Service/Course/Response.cs
+++ KimChiTalk.Service/Course/Response.cs
@@ -2,5 +2,13 @@ namespace KimChiTalk.Service.Course;
 
 public class Response
 {
-    
+    public class CourseResponse
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Level { get; set; }
+        public int Order { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
 }
