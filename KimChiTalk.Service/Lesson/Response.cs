92d1d353c583a291c46a3a74dfe3f97e030cc069
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-25 21:24:10 +0700

    commit and push code

--- KimChiTalk.Service/Lesson/Response.cs
+++ KimChiTalk.Service/Lesson/Response.cs
@@ -2,5 +2,9 @@ namespace KimChiTalk.Service.Lesson;
 
 public class Response
 {
+    public Guid LessonId { get; set; }
+    public required string Title { get; set; }
+    public bool IsEnrolled { get; set; }
+    
     
 }
