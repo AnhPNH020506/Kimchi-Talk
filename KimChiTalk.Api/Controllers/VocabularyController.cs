5ee3964854dc86b72259554cd9395b8fe8f6035e
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-26 22:49:20 +0700

    update file code vocabulary controller

---
+++ KimChiTalk.Api/Controllers/VocabularyController.cs
@@ -0,0 +1,33 @@
+using KimChiTalk.Extensions;
+using KimChiTalk.Service.Vocabulary;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KimChiTalk.Controllers;
+[ApiController]
+[Route("api/v1/vocabulary")]
+public class VocabularyController: ControllerBase
+{
+    private readonly IService _vocabularyService;
+
+    public VocabularyController(IService service)
+    {
+        _vocabularyService = service;
+    }
+
+    [HttpGet()]
+    [Authorize(Policy = JwtExtensions.CustomerPolicy)]
+    public async Task<IActionResult> GetVocabularies( Guid lessonId)
+    {
+        var userId = GetCurrentUserId();
+        if (userId == null)
+            return BadRequest();
+        var result = await _vocabularyService.GetVocabularies(userId!.Value, lessonId);
+        return Ok(result);
+    }
+    private Guid? GetCurrentUserId()
+    {
+        var value = User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+        return Guid.TryParse(value, out var id) ? id : null;
+    }
+}
