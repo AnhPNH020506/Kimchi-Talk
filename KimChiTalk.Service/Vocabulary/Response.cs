4f1fda6d1d72a74829141c1f806b73fa5e7f0aa6
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-26 12:19:34 +0700

    commit new code vocabulary

--- KimChiTalk.Service/Vocabulary/Response.cs
+++ KimChiTalk.Service/Vocabulary/Response.cs
@@ -2,5 +2,15 @@ namespace KimChiTalk.Service.Vocabulary;
 
 public class Response
 {
+    public class GetVocabularies
+    {
+        public Guid Id { get; set; }
+        public required string Word { get; set; }
+        public required string Level { get; set; }
+        public required string Type { get; set; }
+        public required string MeaningVietnamese { get; set; }
+        public bool IsLearned { get; set; }
+        public bool IsFavorite { get; set; }
+    }
     
 }
