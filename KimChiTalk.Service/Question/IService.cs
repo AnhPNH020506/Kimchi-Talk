ce5e3d9d407b02fc7a4e63514e9e148d3afdf40a
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-27 23:54:10 +0700

    update source code service for question

--- KimChiTalk.Service/Question/IService.cs
+++ KimChiTalk.Service/Question/IService.cs
@@ -5,4 +5,5 @@ namespace KimChiTalk.Service.Question;
 public interface IService
 {
     public Task<List<Response.GetQuestionResponse>> GetQuestions(Guid lessonId, QuestionStage questionStage);
+    public Task<Response.SubmitQuestionResponse> SubmitQuestions(Request.SubmitQuestionRequest request, Guid userId);
 }
--- KimChiTalk.Service/Question/Request.cs
+++ KimChiTalk.Service/Question/Request.cs
@@ -1,6 +1,18 @@
+using KimChiTalk.Repository.Entity;
+
 namespace KimChiTalk.Service.Question;
 
 public class Request
 {
-    
+    public class SubmitQuestionRequest
+    {
+        public Guid LessonId { get; set; }
+        public QuestionStage QuestionStage { get; set; }
+        public required List<AnswerSubmission> Answers { get; set; }
+    }
+    public class AnswerSubmission
+    {
+        public Guid QuestionId { get; set; }
+        public required List<Guid> SelectedAnswerIds { get; set; }
+    }
 }
