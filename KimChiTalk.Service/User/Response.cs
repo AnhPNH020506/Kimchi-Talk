b74c7b7a233fcdc899419b8d6ba0c6bf990edd0d
AnhPNH020506 <phamnguyenhunganh475@gmail.com>
2026-08-29 23:43:23 +0700

    update response for new api

--- KimChiTalk.Service/User/Response.cs
+++ KimChiTalk.Service/User/Response.cs
@@ -2,5 +2,19 @@ namespace KimChiTalk.Service.User;
 
 public class Response
 {
-    
+    public class CourseProgressResponse
+    {
+        public Guid CourseId {get; set;}
+        public required string CourseTitle {get; set;}
+        public required string Level {get; set;}
+        public required List<LessonProgressResponse> Lessons {get; set;}
+    }
+
+    public class LessonProgressResponse
+    {
+        public Guid LessonId { get; set; }
+        public required string Title {get; set;}
+        public int Order {get; set;}
+        public bool IsCompleted {get; set;}
+    }
 }
